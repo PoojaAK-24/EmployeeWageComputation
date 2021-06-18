d325dd7a324516e86f6329ee120c988037c55eac
PoojaAK_24 <pooja12reddy@gmail.com>
2021-06-18 13:05:30 +0530

    Added part time Employee

--- empwage.sh
+++ empwage.sh
@@ -1,6 +1,7 @@
 #/bin/bash -x
 echo "Welcome to Employee Wage Computation Problem"
 
+isPartTime=2
 isFullTime=1
 isAbsent=0
 salaryPerHr=20
@@ -11,6 +12,10 @@ if [ $attendance -eq $isFullTime ]
 then
 	fullTimeHr=8
 	echo "Employee is Present"
+elif [ $attendence -eq $isPartTime ]
+then
+	fullTimeHr=4
+	echo "Employee is PartTime present"
 else
 	fullTimeHr=0
 	echo "Employee is Absent"
