89987d4503e06bf798204a46eb44a4ff08aa4c18
PoojaAK_24 <pooja12reddy@gmail.com>
2021-06-18 12:45:56 +0530

    checking employee present or absent

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,13 @@
 #/bin/bash -x
 echo "Welcome to Employee Wage Computation Problem"
 
+isFullTime=1
+isAbsent=0
+attendance=$((RANDOM%2))
+
+if [ $attendance -eq $isFullTime ]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
