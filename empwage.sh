a82e15ea6fd1a66c44d28e91228afb71b241fd2e
PoojaAK_24 <pooja12reddy@gmail.com>
2021-06-18 12:55:34 +0530

    calculating EmpWage

--- empwage.sh
+++ empwage.sh
@@ -3,11 +3,18 @@ echo "Welcome to Employee Wage Computation Problem"
 
 isFullTime=1
 isAbsent=0
+salaryPerHr=20
+totalSalary=0
 attendance=$((RANDOM%2))
 
 if [ $attendance -eq $isFullTime ]
 then
+	fullTimeHr=8
 	echo "Employee is Present"
 else
+	fullTimeHr=0
 	echo "Employee is Absent"
 fi
+
+totalSalary=$(($totalSalary+($salaryPerHr*$fullTimeHr)))
+echo "Salary is: " $totalSalary
