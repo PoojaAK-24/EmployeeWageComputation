8d66a4f5b67bc7a0bab46ed1c89ef30cff8fcdfb
PoojaAK_24 <pooja12reddy@gmail.com>
2021-06-18 13:30:15 +0530

    Solve Using Switch Case Statement

--- empwage.sh
+++ empwage.sh
@@ -1,25 +1,33 @@
-#/bin/bash -x
-echo "Welcome to Employee Wage Computation Problem"
-
-isPartTime=2
+#!/bin/bash -x
 isFullTime=1
 isAbsent=0
-salaryPerHr=20
+isPartTime=2
+FullTimeHr=8
+PartTimeHr=4
+SalaryPerHr=20
 totalSalary=0
-attendance=$((RANDOM%2))
+attendance=$((RANDOM%3))
 
-if [ $attendance -eq $isFullTime ]
-then
-	fullTimeHr=8
-	echo "Employee is Present"
-elif [ $attendence -eq $isPartTime ]
-then
-	fullTimeHr=4
-	echo "Employee is PartTime present"
-else
-	fullTimeHr=0
-	echo "Employee is Absent"
-fi
-
-totalSalary=$(($totalSalary+($salaryPerHr*$fullTimeHr)))
-echo "Salary is: " $totalSalary
+case $attendance in 
+	1) 
+	echo "Full time present"
+		;;
+	2)
+	echo "Part time present"
+		;;
+	*)
+	echo "is absent"
+		;;
+esac
+case $attendance in
+	1)
+	totalSalary=$(($totalSalary+($SalaryPerHr*$FullTimeHr)))
+        	;;
+	2)
+        totalSalary=$(($totalSalary+($SalaryPerHr*$PartTimeHr)))
+		;;
+        *)
+        totalSalary=0
+		;;
+esac
+echo "Salary is: $totalSalary"
